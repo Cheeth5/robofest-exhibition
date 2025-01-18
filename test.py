5fb8c267b63dade21cedeb2a4c0cdf8b8caeaf27
cheeth_chouk <cheethchouk@gmail.com>
2025-01-18 17:03:45 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,37 +1,30 @@
 from ultralytics import YOLO
 import cv2
 
-# Load the YOLO model
 model = YOLO('new.pt')
 
-# Initialize the webcam
-cap = cv2.VideoCapture(0)  # Use 0 for the default camera, or change the index for other cameras
+cap = cv2.VideoCapture(0) 
 
-# Check if the webcam is opened successfully
+
 if not cap.isOpened():
     print("Error: Could not open webcam.")
     exit()
 
 while True:
-    # Read a frame from the webcam
+
     ret, frame = cap.read()
     if not ret:
         print("Error: Failed to capture frame.")
         break
 
-    # Perform inference on the frame
     results = model(frame)
 
-    # Visualize the results (if your YOLO version supports rendering)
     annotated_frame = results[0].plot()
 
-    # Display the annotated frame
-    cv2.imshow("YOLO Real-Time Detection", annotated_frame)
+    cv2.imshow("frame", annotated_frame)
 
-    # Exit the loop when 'q' is pressed
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-# Release the webcam and close the OpenCV window
 cap.release()
 cv2.destroyAllWindows()
